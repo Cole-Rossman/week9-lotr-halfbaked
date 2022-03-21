59d016a2fdfe9c1eaf36f0797125f9ac4968f667
Cole Rossman <colerossman99@gmail.com>
2022-03-21 14:40:37 -0700

    Character.js use effect implemented and return characters with names

---
+++ src/views/Characters/Characters.js
@@ -0,0 +1,31 @@
+
+import React, { useEffect, useState } from 'react';
+import { fetchCharacters } from '../../services/characters';
+
+export default function Characters() {
+  const [characters, setCharacters] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const characterData = await fetchCharacters('All');
+        console.log(characterData);
+        setCharacters(characterData);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className='character'>
+      <h3>Characters</h3>
+      {error && <p>{error}</p>}
+      {characters.map((character) => (
+        <p key={character.id}>{character.name}</p>
+      ))}
+    </div>
+  );
+}
